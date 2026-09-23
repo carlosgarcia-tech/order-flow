130767dc5ed98d52833f2bf7eab8be90073d3412
Carlos G <cgarciagarcia729@gmail.com>
2026-09-23 17:05:19 -0600

    feat(user-service): scaffolding y modelo de dominio (User, Role, repositorio y datos iniciales)

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, email, password, role, enabled) VALUES
+('admin', 'admin@orderflow.com', '$2a$10$sZmX1dJyR5cp7BLv7W8RvezamttWVaQE/Y4uMb46vjcoAcJWwVMf.', 'ROLE_ADMIN', true),
+('user', 'user@orderflow.com', '$2a$10$sZmX1dJyR5cp7BLv7W8RvezamttWVaQE/Y4uMb46vjcoAcJWwVMf.', 'ROLE_USER', true);
